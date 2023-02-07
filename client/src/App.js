369b2dc1b88c766f55f810a07e87b2c659c3ae89
Cariny <perezcariny@gmail.com>
2023-02-07 08:11:58 -0800

    feat: add routing and a homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+import background from '../assets/background.jpeg';
+import './HomePage.scss';
+import { Link } from "react-router-dom";
+
+const HomePage = (props) => {
+	return(
+		<div className='homepage'>
+			<div className='header'>
+				<h1>STAR WARS</h1>
+				<p>AN INTERACTIVE GALACTIC EXPERIENCE</p>
+				<div className='navigation'>
+					<Link to='/characters'>Characters</Link>
+					<Link to='/films'>Films</Link>
+				</div>
+			</div>
+			
+			<img src={background} alt='star-wars background'/>
+		</div>
+	)
+}
+export default HomePage; 
