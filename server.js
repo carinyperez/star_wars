b5dc61ab53b9cd40f069daabfe65e89344a5480c
Cariny <perezcariny@gmail.com>
2023-02-09 16:03:49 -0800

    fix: serve static assets in production

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.use('/api/films', films);
 // cors error 
 app.use('*', cors())
 
+// serve static assets in production 
+if(process.env.NODE_ENV === 'production') {
+    // set static folder 
+    app.use(express.static('client/build')); 
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
 
